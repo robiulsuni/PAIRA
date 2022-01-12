96d2d679e4f98270b606e82708152686f8c637bf
Md. Robiul Suni <sunirobiul@gmail.com>
2022-01-12 15:59:26 +0600

    kotlin version

--- android/app/src/main/kotlin/com/example/paira/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/paira/MainActivity.kt
@@ -3,4 +3,8 @@ package com.example.paira
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    plugins {
+  kotlin("<...>") version "1.6.10"
+}
+    
 }
