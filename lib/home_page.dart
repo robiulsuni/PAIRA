c8778002fe75571db813a677c102e3097a1ea93e
Md. Robiul Suni <85034871+robiulsuni@users.noreply.github.com>
2022-01-12 11:53:49 +0600

    s

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home Page'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.green,
+                width: MediaQuery.of(context).size.width * .42,
+                height: 200.0,
+                child: Text('HomePages'),
+              )
+            ],
+          ),
+        ));
+  }
+}
