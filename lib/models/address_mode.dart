7d62427921920478dcf3dd0f210e9042ea800a39
Md. Robiul Suni <85034871+robiulsuni@users.noreply.github.com>
2022-01-18 12:28:27 +0600

    json file create

---
+++ lib/models/address_mode.dart
@@ -0,0 +1,16 @@
+class AddressModel {
+  int? id;
+  String? street;
+  String? suite;
+  String? city;
+  String? zipcode;
+
+  AddressModel({this.id, this.street, this.suite, this.city, this.zipcode});
+
+  AddressModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    street = json['street'];
+    suite = json['city'];
+    city = json['zipcode'];
+  }
+}
