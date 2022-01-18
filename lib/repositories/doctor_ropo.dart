c57d48629ac92f49ac89317a1860ac78257b1de6
Md. Robiul Suni <85034871+robiulsuni@users.noreply.github.com>
2022-01-18 13:08:01 +0600

    doctor model

---
+++ lib/repositories/doctor_ropo.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class DoctorRepo {
+  static Future<List<DoctorModel>> getDoctorList() async {
+    final respose = await rootBundle.loadString('assets/json/doctor_list.json');
+    if (respose != null) {
+      List<dynamic> doctors = await json.decode(respose);
+      List<DoctorModel> doctorList =
+          doctors.map((e) => DoctorModel.fromJson(e)).toList();
+      return doctorList;
+    } else {
+      return [];
+    }
+  }
+}
